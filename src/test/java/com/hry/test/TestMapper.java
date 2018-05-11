feecfb28030a120906501e053633c939ab3726fe
MonkeyBrother <2507511047@qq.com>
2018-05-11 23:15:00 +0800

    配置好了spring单元测试，还没有进行实际测试mapper是否可用

---
+++ src/test/java/com/hry/test/TestMapper.java
@@ -0,0 +1,35 @@
+package com.hry.test;
+
+import com.hry.ssm.mapper.UserMapper;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.UUID;
+
+@ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestMapper {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+    @Test
+    public void testCurd() {
+        SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession(ExecutorType.BATCH, false);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        for (int i = 0; i < 1000; i++) {
+            String uname = UUID.randomUUID().toString().substring(0, 5).replace("-", "");
+//            mapper.insert(new User(null, uname, "123"));
+            sqlSession.commit();
+        }
+        System.out.println("批量成功");
+    }
+
+}
+
